4c8231f882f4875a36af03a2cc83ab261bbec13a
dwiyudha0444 <yudhacamo12@gmail.com>
2023-07-22 19:21:41 +0700

    Dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::resource('/home',HomeController::class);
+Route::resource('/admin',AdminController::class);
