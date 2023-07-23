f0a74965bcba45f0e279910ccbb292a98154985c
dwiyudha0444 <yudhacamo12@gmail.com>
2023-07-23 10:48:14 +0700

    view update

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\client\HomeController;
+use App\Http\Controllers\admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
