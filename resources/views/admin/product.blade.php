6dea4331431d6f9636050461392aa502b1b9a210
dwiyudha0444 <yudhacamo12@gmail.com>
2023-07-23 19:13:06 +0700

    Table Product

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -1,91 +1,101 @@
 <div class="content-wrapper">
+
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>DataTables</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">DataTables</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>DataTables</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">DataTables</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
     </section>
 
     <!-- Main content -->
     <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-        
-            <!-- /.card -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
 
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">DataTable with default features</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
-                  <thead>
-                  <tr>
-                    <th>Rendering engine</th>
-                    <th>Browser</th>
-                    <th>Platform(s)</th>
-                    <th>Engine version</th>
-                    <th>CSS grade</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                    <td>Trident</td>
-                    <td>Internet
-                      Explorer 4.0
-                    </td>
-                    <td>Win 95+</td>
-                    <td> 4</td>
-                    <td>X</td>
-                  </tr>
-                  
-                  <tr>
-                    <td>Misc</td>
-                    <td>PSP browser</td>
-                    <td>PSP</td>
-                    <td>-</td>
-                    <td>C</td>
-                  </tr>
-                  <tr>
-                    <td>Other browsers</td>
-                    <td>All others</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>U</td>
-                  </tr>
-                  </tbody>
-                  <tfoot>
-                  <tr>
-                    <th>Rendering engine</th>
-                    <th>Browser</th>
-                    <th>Platform(s)</th>
-                    <th>Engine version</th>
-                    <th>CSS grade</th>
-                  </tr>
-                  </tfoot>
-                </table>
-              </div>
-              <!-- /.card-body -->
+                    <!-- /.card -->
+
+                    <div class="card">
+                        <div class="card-header mt-3">
+                            <div class="row">
+                                <div class="col-3">
+                                    <button type="button" class="btn btn-block bg-primary">Tambah</button>
+                                </div>
+                                <div class="col-3">
+
+                                </div>
+                                <div class="col-5">
+                                    <section class="content">
+                                        <div class="container-fluid">
+                                            <div class="row">
+                                                <div class="col-md-8 offset-md-6">
+                                                    <form action="simple-results.html">
+                                                        <div class="input-group">
+                                                            <input type="search" class="form-control form-control-lg"
+                                                                placeholder="Type your keywords here">
+                                                            <div class="input-group-append">
+                                                                <button type="submit" class="btn btn-lg btn-default">
+                                                                    <i class="fa fa-search"></i>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                          <table id="example1" class="table table-bordered table-striped ">
+                              <thead>
+                                  <tr>
+                                      <th>Nama</th>
+                                      <th>Jenis</th>
+                                      <th>Harga</th>
+                                      <th>Action</th>
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                  <tr>
+                                      <td>Data 1</td>
+                                      <td>Data 2</td>
+                                      <td>Data 3</td>
+                                      <td>
+                                        <div class="d-flex justify-content-between">
+                                          <button type="button" class="btn btn-block bg-gradient-success ">Edit</button>
+                                          <button type="button" class="btn btn-block bg-gradient-success ">Edit</button>
+                                        </div>
+                                        </td>
+                                  
+                                  </tr>
+                                  <!-- Add more rows as needed -->
+                              </tbody>
+                          </table>
+                      </div>
+                      
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
             </div>
-            <!-- /.card -->
-          </div>
-          <!-- /.col -->
+            <!-- /.row -->
         </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
+        <!-- /.container-fluid -->
     </section>
     <!-- /.content -->
-  </div>
+</div>
